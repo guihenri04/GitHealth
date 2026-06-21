1ac48a697d06fabd6b9da56d9e48b517cdd41cbb
caiosgrossi <caiogrossi.br@gmail.com>
2026-06-20 23:58:00 -0300

    feat: add file hotspot model

--- src/githealth/models/file_change.py
+++ src/githealth/models/file_change.py
@@ -11,3 +11,14 @@ class FileChange:
     deletions: int
     changes: int
 
+
+@dataclass(frozen=True)
+class FileHotspot:
+    filename: str
+    pull_requests: int
+    median_first_review_hours: float | None
+    median_cycle_hours: float | None
+    median_review_effort: float
+    changes_requested: int
+    sample_sufficient: bool
+
