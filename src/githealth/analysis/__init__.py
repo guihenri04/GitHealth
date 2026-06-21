6775c04f660d9f221f2bae05f5b020bc5f80d92f
caiosgrossi <caiogrossi.br@gmail.com>
2026-06-21 16:12:00 -0300

    feat: calculate percentile based scores

--- src/githealth/analysis/__init__.py
+++ src/githealth/analysis/__init__.py
@@ -1,6 +1,8 @@
 from githealth.analysis.durations import build_all_metrics, build_pull_request_metrics
+from githealth.analysis.scores import apply_scores
 
 __all__ = [
+    "apply_scores",
     "build_all_metrics",
     "build_pull_request_metrics",
 ]
