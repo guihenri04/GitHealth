da18627de43ee83c88514193927a26eeb3e8c1b6
caiosgrossi <caiogrossi.br@gmail.com>
2026-06-21 17:00:00 -0300

    feat: calculate spearman correlations

--- src/githealth/analysis/__init__.py
+++ src/githealth/analysis/__init__.py
@@ -1,8 +1,11 @@
+from githealth.analysis.correlations import CorrelationResult, calculate_correlations
 from githealth.analysis.durations import build_all_metrics, build_pull_request_metrics
 from githealth.analysis.scores import apply_scores
 
 __all__ = [
+    "CorrelationResult",
     "apply_scores",
     "build_all_metrics",
     "build_pull_request_metrics",
+    "calculate_correlations",
 ]
