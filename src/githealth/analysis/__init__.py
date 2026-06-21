8581837fa7e2c5423ea138ba80c4eadf4d7dbb04
caiosgrossi <caiogrossi.br@gmail.com>
2026-06-21 17:40:00 -0300

    feat: rank file hotspots

--- src/githealth/analysis/__init__.py
+++ src/githealth/analysis/__init__.py
@@ -1,5 +1,6 @@
 from githealth.analysis.correlations import CorrelationResult, calculate_correlations
 from githealth.analysis.durations import build_all_metrics, build_pull_request_metrics
+from githealth.analysis.hotspots import calculate_file_hotspots
 from githealth.analysis.scores import apply_scores
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "build_all_metrics",
     "build_pull_request_metrics",
     "calculate_correlations",
+    "calculate_file_hotspots",
 ]
